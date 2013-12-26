f7360cc2fe1ab484259e8660f1447de66cd6ba2e
Keith Dimatteo <keith@hauthaus.net>
2013-12-26 17:49:03 -0500

    remove some testing code

--- web.js
+++ web.js
@@ -27,25 +27,12 @@ appInstance.csvConverter = new converter();
 appInstance.csvConverter.from(appInstance.csvFileName);
 appInstance.ee = new eventEmitter();
 
-/**/
-//Converter Class
-//var Converter = require("csvtojson").core.Converter;
-
-//CSV File Path or CSV String or Readable Stream Object
-//var csvFileName = "./raw.csv";
-
-//new converter instance
-//appInstance.csvConverter = new Converter();
-
 //end_parsed will be emitted once parsing finished
 appInstance.csvConverter.on("end_parsed", function(jsonObj){
 	"use strict"; // linter ;_; 
 
 	appInstance.dataObj = jsonObj;
 	appInstance.ee.emit("end_parsed");
-	//console.log(appInstance);
-    //console.log(jsonObj); //here is your result json object
-    //this.dataObj = jsonObj;
 
 });
 
@@ -64,38 +51,3 @@ appInstance.ee.on('end_parsed', function(){
 	appInstance.expressApp.listen(port);
 
 });
-
-/*
-appInstance.expressApp.get('/', function(req, res){
-	res.send("boz");
-});
-
-appInstance.expressApp.listen(8008);
-*/
-
-
-/*
-app.get('/', function(req, res){
-	res.send(this.dataObj);
-});
-
-app.listen(8008);
-*/
-
-//read from file
-//appInstance.csvConverter.from(csvFileName);
-
-
-
-
-
-/**
- * @see http://www.senchalabs.org/connect/
- */
- /*
-var connect = require('connect');
-var app = connect()
-	.use(connect.basicAuth('username', 'password'))
-	.use(connect.static('www'))
-	.listen(process.env.PORT || 8080)
-*/
